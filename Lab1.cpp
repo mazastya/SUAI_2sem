33912145c8cbfd5cc0806a4812d1310035cf48a1
Anastasia <104169838+mazastya@users.noreply.github.com>
2022-04-25 20:50:49 +0300

    lab1

---
+++ Lab1.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main() {
+
+	unsigned char a, b, c, d;
+	a = 74 & 111;
+	printf("74 AND 111 = (74) = %d\n", a);
+	b = 74 ^ 111;
+	printf("74 XOR 111 = (37) = %d\n", b);
+	c = 74 >> 1;
+	printf("74 >> 1 = (37) = %d\n", c);
+	d = 28 | -74 & (~36 ^ -28);
+	printf("28 OR -74 AND (NOT 36 XOR -28) = (62) = %d\n", d);
+
+	return 0;
+}
